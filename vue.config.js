28eb1664738f5795151919c16d63a650c49e323f
leventekobor94 <kobor.levente@tigra.hu>
2021-03-16 22:11:21 +0100

    add baseUrl to vue config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  publicPath: '/https://github.com/leventekobor94/redmine-kanban//'
+  publicPath: '/https://github.com/leventekobor94/redmine-kanban/',
+  baseUrl: '/https://github.com/leventekobor94/redmine-kanban/'
 }
