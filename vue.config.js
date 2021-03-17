f944a02f3e59a460dcfac38919522a48eda1190c
leventekobor94 <kobor.levente@tigra.hu>
2021-03-17 21:41:06 +0100

    fix vue config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-  publicPath: '/https://github.com/leventekobor94/redmine-kanban/',
-  baseUrl: '/https://github.com/leventekobor94/redmine-kanban/'
+  publicPath: '/redmine-kanban/'
 }
