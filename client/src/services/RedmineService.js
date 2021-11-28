0daf5562652d255ccee12f3d8d8a91d960fdcfc0
lorikViktor <asd>
2021-11-28 16:31:29 +0100

    remove unused methods from RedmineService.js

--- client/src/services/RedmineService.js
+++ client/src/services/RedmineService.js
@@ -11,16 +11,6 @@ export default class RedmineService{
       }
     })
   }
-  static getIssues(apiKey, issueId) {
-    return Api().get('issues.json', {
-      headers: {
-        'X-Redmine-API-Key': apiKey
-      },
-      params: {
-        'issue_id': issueId
-      }
-    })
-  }
   static getProjects(apiKey, offset) {
     return Api().get('projects.json', {
       headers: {
@@ -43,19 +33,6 @@ export default class RedmineService{
       }
     })
   }
-  static getAllUpdatedIssuesIn2020(apiKey, offset) {
-    return Api().get('issues.json', {
-      headers: {
-        'X-Redmine-API-Key': apiKey
-      },
-      params: {
-        'updated_by': 'me',
-        'updated_on' : '><2020-01-01|2020-12-31',
-        'limit': 100,
-        'offset': offset
-      }
-    })
-  }
   static getIssuesForProject(apiKey, queryId, projectId) {
     return Api().get('issues.json', {
       headers: {
@@ -65,20 +42,6 @@ export default class RedmineService{
         'project_id' : projectId,
         'query_id' : queryId,
         'limit': 100
-      }
-    })
-  }
-  static getAllTimeEntriesIn2020(apiKey, offset) {
-    return Api().get('time_entries.json', {
-      headers: {
-        'X-Redmine-API-Key': apiKey
-      },
-      params: {
-        'user_id' : 'me',
-        'from' : '2020-01-01',
-        'to' : '2020-12-31',
-        'limit': 100,
-        'offset': offset
       }
     })
   }
